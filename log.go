7964db6a2ee7b1cdaf48e17225bb6d3d1febe09c
Clement <clement.rabourdin@myeggo.com>
2019-06-03 01:37:18 +0200

    Add date/time  to  logs

--- log.go
+++ log.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 var logURL string
@@ -12,21 +13,29 @@ func log(err error) {
 		return
 	}
 
+	// Get time and format it
+	now := fmt.Sprintf("%s", time.Now().Format("2006-01-02T15:04:05"))
+	// Stringify error
 	strErr := fmt.Sprintf("%s", err)
+	//  Get log file URL
 	if logURL != "" {
 		getLogURL()
 	}
 
+	// Open log file
 	f, err := os.OpenFile(logURL, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error Can't open %s\nOriginal error: %s", logURL, strErr)
+		// Error opening log file write to Stderr
+		fmt.Fprintf(os.Stderr, "%s - Error Can't open %s\nOriginal error: %s", now, logURL, strErr)
 		return
 	}
 	defer f.Close()
 
-	_, err = f.WriteString(strErr)
+	// Write to log file
+	_, err = f.WriteString(now + " - " + strErr)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error Can't wrtite to %s\nOriginal error: %s", logURL, strErr)
+		// Error while writting to log file, write to Stderr
+		fmt.Fprintf(os.Stderr, "%s - Error Can't wrtite to %s\nOriginal error: %s", now, logURL, strErr)
 		return
 	}
 }
